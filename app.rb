43dc979ead047b48ef92fc78554ad2d3ea65aade
Maynard Hartman <mjhartman@brighthouse.com>
2020-02-21 18:05:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Maynard"
+  end
+  
+  get '/hometown' do
+    "My hometowm is Bloomsburg"
+  end
+  
+  get '/favorite_song' do
+    "My favorite song is Fame"
+  end
+  
 end
