b98523c63606843ac0582a83dfd273f549c7e8be
Maynard Hartman <mjhartman@brighthouse.com>
2020-02-21 18:13:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ class App < Sinatra::Base
     "My name is \_\_"
   end
   
-  get '/name/hometown' do
+  get '/hometown' do
     "My hometowm is \_\_"
   end
   
-  get '/name/favorite_song' do
+  get '/favorite-song' do
     "My favorite song \_\_"
   end
   
